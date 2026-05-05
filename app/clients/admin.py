ef45e5632461cab34d3174faa11eb341370ac5e4
vladiuse <vladiuse@gmail.com>
2026-05-05 22:23:54 +0300

    add field and admin to clients

--- app/clients/admin.py
+++ app/clients/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from clients.models import Client, ClientAccount
+
+
+class ClientAccountInline(admin.TabularInline):
+    model = ClientAccount
+    extra = 0
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("name", "source_name", "is_blocked")
+    search_fields = ("name", "source_name")
+    list_filter = ("is_blocked",)
+    inlines = (ClientAccountInline,)
+
+
+@admin.register(ClientAccount)
+class ClientAccountAdmin(admin.ModelAdmin):
+    list_display = ("user", "client", "role")
+    search_fields = ("user__username", "client__name")
+    list_filter = ("role",)
--- app/clients/models.py
+++ app/clients/models.py
@@ -4,6 +4,7 @@ from users.models import User
 
 class Client(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    source_name = models.CharField(max_length=255, unique=True)
     is_blocked = models.BooleanField(default=False)
 
     def __str__(self) -> str:
