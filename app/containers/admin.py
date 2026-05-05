de53e636b20f205df3d91749d5942af89dce245b
vladiuse <vladiuse@gmail.com>
2026-05-05 21:59:50 +0300

    add model

--- app/containers/admin.py
+++ app/containers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from containers.models import Container
+
+
+@admin.register(Container)
+class ContainerAdmin(admin.ModelAdmin):
+    list_display = ("number", "client_name", "start_date", "end_date", "area")
+    search_fields = ("number", "client_name", "send_number")
+    list_filter = ("area",)
+    readonly_fields = ("number",)
--- app/containers/models.py
+++ app/containers/models.py
@@ -1,3 +1,16 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Container(models.Model):
+    number = models.CharField(max_length=11)
+    client_name = models.CharField(max_length=30)
+    start_date = models.DateTimeField(default=None, null=True)
+    end_date = models.DateTimeField(default=None, null=True)
+    nn = models.CharField(max_length=5, blank=True)
+    send_number = models.CharField(max_length=10, blank=True)
+    weight = models.CharField(max_length=5, blank=True)
+    area = models.PositiveIntegerField(default=None, null=True, validators=[MaxValueValidator(99)])
+
+    def __str__(self) -> str:
+        return f"<Container:{self.number}>"
