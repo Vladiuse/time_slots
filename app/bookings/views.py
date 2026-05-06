5935e38e08e966d6280c8707a6678bf09d0affee
vladiuse <vladiuse@gmail.com>
2026-05-07 00:54:51 +0300

    кулл

--- app/bookings/views.py
+++ app/bookings/views.py
@@ -1,9 +1,22 @@
+from containers.models import Container
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect
 from django.views import View
+
+from .models import Booking, Slot
 
 
 class BookingCreateView(View):
     def post(self, request: HttpRequest) -> HttpResponse:
         slot_id = request.POST.get("slot_id")
         container_numbers = request.POST.getlist("container_numbers")
-        return HttpResponse(f"slot_id={slot_id}, containers={container_numbers}")
+
+        slot = Slot.objects.get(pk=slot_id)
+        containers = Container.objects.filter(number__in=container_numbers)
+
+        bookings = [Booking(slot=slot, container=container) for container in containers]
+        Booking.objects.bulk_create(bookings)
+
+        containers.update(status=Container.Status.IN_BOOKING)
+
+        return redirect("containers:containers_list")
