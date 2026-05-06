e4d4ad222769240df3042f683d8d6cd3970fcd96
vladiuse <vladiuse@gmail.com>
2026-05-07 01:00:29 +0300

    кулл

--- app/containers/views.py
+++ app/containers/views.py
@@ -1,7 +1,6 @@
-from datetime import date
-
 from bookings.models import Booking, Slot
 from clients.models import Client
+from django.db.models import Count, F, Q
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
@@ -11,7 +10,17 @@ from .models import Container
 
 def containers_list(request: HttpRequest) -> HttpResponse:
     client = Client.objects.get(source_name='ООО "КРАФТТРАНС"')
-    slots = Slot.objects.filter(date=timezone.localdate()).order_by("start_time")
+    slots = (
+        Slot.objects.filter(date=timezone.localdate())
+        .annotate(
+            booking_count=Count(
+                "bookings",
+                filter=~Q(bookings__status=Booking.Status.CANCELLED),
+            ),
+        )
+        .annotate(total_count=F("current_count") + F("booking_count"))
+        .order_by("start_time")
+    )
     containers = Container.objects.filter(client=client).exclude(status=Container.Status.PICKED_UP)
     content = {
         "containers": containers,
