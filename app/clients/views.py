e07da6cda9d089300742d994feac41a1c9bd1419
vladiuse <vladiuse@gmail.com>
2026-05-07 23:38:58 +0300

    ч

--- app/clients/views.py
+++ app/clients/views.py
@@ -1,15 +1,20 @@
 from containers.models import Container
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Count, Q
+from django.db.models import Count, OuterRef, Q, Subquery
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import View
 
-from .models import Client
+from .models import Client, ClientAccount
 
 
 class ClientListView(LoginRequiredMixin, View):
     def get(self, request: HttpRequest) -> HttpResponse:
+        master_username = ClientAccount.objects.filter(
+            client=OuterRef("pk"),
+            role=ClientAccount.Role.MASTER,
+        ).values("user__username")[:1]
+
         clients = Client.objects.annotate(
             containers_on_station=Count(
                 "containers",
@@ -19,5 +24,6 @@ class ClientListView(LoginRequiredMixin, View):
                 "containers",
                 filter=Q(containers__status=Container.Status.IN_BOOKING),
             ),
+            master_username=Subquery(master_username),
         ).order_by("name")
         return render(request, "clients/client_list.html", {"clients": clients})
