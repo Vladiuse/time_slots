5374f8aa3f73cab769f8a2b1ff04afb942e01143
vladiuse <vladiuse@gmail.com>
2026-05-13 17:11:27 +0300

    ч

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/containers/", permanent=False)),
     path("admin/", admin.site.urls),
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
